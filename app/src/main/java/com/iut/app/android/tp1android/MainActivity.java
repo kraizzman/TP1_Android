19b56b6839136213c9a311fc6040a3aa0d4dd854
Johan Gagean <johan.gagean@gmail.com>
2023-02-21 00:54:59 +0100

    Remove second activity. Clean names of id's. Add DataPicker for the birthdate.

--- app/src/main/java/com/iut/app/android/tp1android/MainActivity.java
+++ app/src/main/java/com/iut/app/android/tp1android/MainActivity.java
@@ -1,32 +1,55 @@
 package com.iut.app.android.tp1android;
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.RadioButton;
 
-import java.io.Console;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-    final static String KEY_NAME = "name";
-    final static String KEY_FIRST_NAME = "name";
+
+    final Calendar myCalendar= Calendar.getInstance();
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText=(EditText) findViewById(R.id.til_birthday);
+        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                updateLabel();
+            }
+        };
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(MainActivity.this,date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
 
-        String name = getIntent().getStringExtra(KEY_NAME);
-        String firstName = getIntent().getStringExtra(KEY_FIRST_NAME);
-
-        //TextView tv_name = findViewById(R.id.tv_name);
+    private void updateLabel(){
+        String myFormat="MM/dd/yy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.FRANCE);
+        editText.setText(dateFormat.format(myCalendar.getTime()));
     }
 
     @Override
@@ -59,17 +82,4 @@ public class MainActivity extends AppCompatActivity {
         Log.e("MainActivity", "onDestroy");
     }
 
-    private void openDetail() {
-        //Intent i = new Intent(getApplicationContext(), DetailActivity())
-        //i.putExtra(DetailActivity.KEY_NAME, "Gagean")
-        //I.putExtra(
-    }
-
-    public void sendMessage(View view) {
-//        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
-    }
 }
